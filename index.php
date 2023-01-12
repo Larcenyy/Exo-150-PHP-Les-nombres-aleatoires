3790c683a2b433ac79651a7a04f1c35c2d3ef5e6
Rémy <remyc59440@hotmail.com>
2023-01-12 14:58:41 +0100

    dacc

--- index.php
+++ index.php
@@ -5,15 +5,15 @@
  */
 
 // TODO Votre code ici.
-
-
-
+$ordi = rand(0, 50);
+echo "<div>$ordi</div>";
 /**
  * 2. Générez un nombre aléatoire compris entre 50 et getrandmax, affichez le résultat dans un nouveau <p>
  */
 
 // TODO Votre code ici.
-
+$ordi2 = rand(50, getrandmax());
+echo "<p>$ordi2</p>";
 
 /**
  * 3. Générez un nombre aléatoire compris entre 0 et 50
@@ -23,12 +23,28 @@
 
 // TODO Votre code ici.
 
+$ordi3 = rand(0, 50);
+
+if ($ordi3 <= 25){
+    echo "<p>vous avez winn</p>";
+}
+else{
+    echo "<p>vous avez perdu</p>";
+}
 
 /**
  * 4. Créez une fonction qui génère un nombre aléatoire compris entre $max et $min ( paramètres ).
  * ==> Si le nombre est compris entre $max et $max -100, regénérez un nouveau nombre avec un appel récursif.
  * ==> Si ce n'est pas le cas, retournez tout simplement le nombre généré.
  */
+
+function mathRandom($max, $min){
+    $rand = rand($max, $min);
+    if ($rand > $max - $max){
+        return mathRandom($max, $min);
+    }
+    return $rand;
+}
 
 // TODO Votre code ici.
 
